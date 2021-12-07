67ad8316b35384d72db3f1894a2cc8ad44c790ba
maxime-huyghe <maxime.huyghe@etu.u-pec.fr>
2021-12-07 18:28:11 +0100

    autoformat

--- virtualKeyboardDetector.js
+++ virtualKeyboardDetector.js
@@ -2,30 +2,35 @@
  *  The Virtual Keyboard Detector
  */
 
-var virtualKeyboardDetector = function( window, undefined ) {
-
+var virtualKeyboardDetector = function (window, undefined) {
   var recentlyFocusedTimeoutDuration = 3000;
 
   var previousViewportWidth, viewportWidthWithoutVirtualKeyboard;
   var previousViewportHeight, viewportHeightWithoutVirtualKeyboard;
-  var currentViewportWidth = previousViewportWidth = viewportWidthWithoutVirtualKeyboard = window.innerWidth;
-  var currentViewportHeight = previousViewportHeight = viewportHeightWithoutVirtualKeyboard = window.innerHeight;
+  var currentViewportWidth =
+    (previousViewportWidth =
+    viewportWidthWithoutVirtualKeyboard =
+      window.innerWidth);
+  var currentViewportHeight =
+    (previousViewportHeight =
+    viewportHeightWithoutVirtualKeyboard =
+      window.innerHeight);
 
   var virtualKeyboardVisible = false;
   var recentlyFocused = false;
   var recentlyFocusedTimeout = null;
-  var validFocusableElements = [ 'INPUT', 'TEXTAREA' ];
+  var validFocusableElements = ["INPUT", "TEXTAREA"];
 
   var subscriptions = {};
-
 
   /**
    * Public functions
    */
 
-  function init ( options ) {
-    if ( typeof options !== 'undefined' ) {
-      if ( typeof options.recentlyFocusedTimeoutDuration !== 'undefined' ) recentlyFocusedTimeoutDuration = options.recentlyFocusedTimeoutDuration;
+  function init(options) {
+    if (typeof options !== "undefined") {
+      if (typeof options.recentlyFocusedTimeoutDuration !== "undefined")
+        recentlyFocusedTimeoutDuration = options.recentlyFocusedTimeoutDuration;
     }
 
     resetViewportSizes();
@@ -33,47 +38,49 @@ var virtualKeyboardDetector = function( window, undefined ) {
     initResizeListener();
   }
 
-  function isVirtualKeyboardVisible () {
+  function isVirtualKeyboardVisible() {
     return virtualKeyboardVisible;
   }
 
-  function getVirtualKeyboardSize () {
-    if ( !virtualKeyboardVisible ) return false;
-    
+  function getVirtualKeyboardSize() {
+    if (!virtualKeyboardVisible) return false;
+
     return {
       width: currentViewportWidth,
-      height: viewportHeightWithoutVirtualKeyboard - currentViewportHeight
+      height: viewportHeightWithoutVirtualKeyboard - currentViewportHeight,
     };
   }
 
   // Subscribe
-  function on ( eventName, fn ) {
-    if ( typeof subscriptions[eventName] === 'undefined' ) subscriptions[eventName] = [];
+  function on(eventName, fn) {
+    if (typeof subscriptions[eventName] === "undefined")
+      subscriptions[eventName] = [];
 
     subscriptions[eventName].push(fn);
   }
 
   // Unsubscribe
-  function off ( eventName, fn ) {
-    if (typeof subscriptions[eventName] === 'undefined' ) return;
+  function off(eventName, fn) {
+    if (typeof subscriptions[eventName] === "undefined") return;
 
-    if (typeof fn === 'undefined') {
+    if (typeof fn === "undefined") {
       subscriptions[eventName] = [];
     } else {
       var i = subscriptions[eventName].length;
-      while ( i-- ) {
-        if ( subscriptions[eventName][i] == fn ) subscriptions[eventName].splice(i, 1);
+      while (i--) {
+        if (subscriptions[eventName][i] == fn)
+          subscriptions[eventName].splice(i, 1);
       }
     }
   }
 
   // Publish
-  function trigger ( eventName, args ) {
-    for ( var i in subscriptions[eventName] ) {
-      if ( typeof subscriptions[eventName][i] === 'function' ) subscriptions[eventName][i]( args );
-    }   
+  function trigger(eventName, args) {
+    for (var i in subscriptions[eventName]) {
+      if (typeof subscriptions[eventName][i] === "function")
+        subscriptions[eventName][i](args);
+    }
   }
-
 
   /**
    * Private functions
@@ -81,66 +88,87 @@ var virtualKeyboardDetector = function( window, undefined ) {
 
   // Reset all sizes. We presume the virtual keyboard is not visible at this stage.
   // We call this function on initialisation, so make sure you initialise the virtualKeyBoardListener at a moment when the virtual keyboard is likely to be invisible.
-  function resetViewportSizes () {
-    currentViewportWidth = previousViewportWidth = viewportWidthWithoutVirtualKeyboard = window.innerWidth;
-    currentViewportHeight = previousViewportHeight = viewportHeightWithoutVirtualKeyboard = window.innerHeight;
+  function resetViewportSizes() {
+    currentViewportWidth =
+      previousViewportWidth =
+      viewportWidthWithoutVirtualKeyboard =
+        window.innerWidth;
+    currentViewportHeight =
+      previousViewportHeight =
+      viewportHeightWithoutVirtualKeyboard =
+        window.innerHeight;
   }
 
   // Initialise the listener that checks for focus events in the whole document. This way we can also handle dynamically added focusable elements.
-  function initFocusListener () {
-    document.addEventListener( 'focus', documentFocusHandler, true );
+  function initFocusListener() {
+    document.addEventListener("focus", documentFocusHandler, true);
   }
 
   // Handle the document focus event. We check if the target was a valid focusable element.
-  function documentFocusHandler (e) {
-    if (typeof e.target !== 'undefined' && typeof e.target.nodeName !== 'undefined') {
-      if (validFocusableElements.indexOf(e.target.nodeName) != -1) elementFocusHandler(e);
+  function documentFocusHandler(e) {
+    if (
+      typeof e.target !== "undefined" &&
+      typeof e.target.nodeName !== "undefined"
+    ) {
+      if (validFocusableElements.indexOf(e.target.nodeName) != -1)
+        elementFocusHandler(e);
     }
   }
 
   // Handle the case when a valid focusable element is focused. We flag that a valid element was recently focused. This flag expires after recentlyFocusedTimeoutDuration.
-  function elementFocusHandler (e) {
-    if ( recentlyFocusedTimeout != null ) {
-      window.clearTimeout( recentlyFocusedTimeout );
+  function elementFocusHandler(e) {
+    if (recentlyFocusedTimeout != null) {
+      window.clearTimeout(recentlyFocusedTimeout);
       recentlyFocusedTimeout = null;
     }
 
     recentlyFocused = true;
 
-    recentlyFocusedTimeout = window.setTimeout( expireRecentlyFocused, recentlyFocusedTimeoutDuration );
+    recentlyFocusedTimeout = window.setTimeout(
+      expireRecentlyFocused,
+      recentlyFocusedTimeoutDuration
+    );
   }
 
-  function expireRecentlyFocused () {
+  function expireRecentlyFocused() {
     recentlyFocused = false;
   }
 
-  function initResizeListener () {
-    window.addEventListener( 'resize', resizeHandler );
+  function initResizeListener() {
+    window.addEventListener("resize", resizeHandler);
   }
 
-  function resizeHandler () {
+  function resizeHandler() {
     currentViewportWidth = window.innerWidth;
     currentViewportHeight = window.innerHeight;
 
     // If the virtual keyboard is tought to be visible, but the viewport height returns to the value before keyboard was visible, we presume the keyboard was hidden.
-    if ( virtualKeyboardVisible && currentViewportWidth == previousViewportWidth && currentViewportHeight >= viewportHeightWithoutVirtualKeyboard ) {
+    if (
+      virtualKeyboardVisible &&
+      currentViewportWidth == previousViewportWidth &&
+      currentViewportHeight >= viewportHeightWithoutVirtualKeyboard
+    ) {
       virtualKeyboardHiddenHandler();
     }
 
     // If the width of the viewport is changed, it's hard to tell wether virtual keyboard is still visible, so we make sure it's not.
-    if ( currentViewportWidth != previousViewportWidth ) {
-      if ( 'activeElement' in document )
-        document.activeElement.blur();
+    if (currentViewportWidth != previousViewportWidth) {
+      if ("activeElement" in document) document.activeElement.blur();
       virtualKeyboardHiddenHandler();
     }
 
     // If recently focused and viewport height is smaller then previous height, we presume that the virtual keyboard has appeared.
-    if ( !virtualKeyboardVisible && recentlyFocused && currentViewportWidth == previousViewportWidth && currentViewportHeight < previousViewportHeight ) {
+    if (
+      !virtualKeyboardVisible &&
+      recentlyFocused &&
+      currentViewportWidth == previousViewportWidth &&
+      currentViewportHeight < previousViewportHeight
+    ) {
       virtualKeyboardVisibleHandler();
     }
 
     // If the keyboard is presumed not visible, we save the current measurements as values before keyboard was shown.
-    if ( virtualKeyboardVisible == false ) {
+    if (virtualKeyboardVisible == false) {
       viewportWidthWithoutVirtualKeyboard = currentViewportWidth;
       viewportHeightWithoutVirtualKeyboard = currentViewportHeight;
     }
@@ -149,42 +177,42 @@ var virtualKeyboardDetector = function( window, undefined ) {
     previousViewportHeight = currentViewportHeight;
   }
 
-  function virtualKeyboardVisibleHandler () {
+  function virtualKeyboardVisibleHandler() {
     virtualKeyboardVisible = true;
 
     var eventData = {
       virtualKeyboardVisible: virtualKeyboardVisible,
-      sizes: getSizesData()
+      sizes: getSizesData(),
     };
 
-    trigger( 'virtualKeyboardVisible', eventData );
+    trigger("virtualKeyboardVisible", eventData);
   }
 
-  function virtualKeyboardHiddenHandler () {
+  function virtualKeyboardHiddenHandler() {
     virtualKeyboardVisible = false;
 
     var eventData = {
       virtualKeyboardVisible: virtualKeyboardVisible,
-      sizes: getSizesData()
+      sizes: getSizesData(),
     };
 
-    trigger( 'virtualKeyboardHidden', eventData );
+    trigger("virtualKeyboardHidden", eventData);
   }
 
-  function getSizesData () {
+  function getSizesData() {
     return {
       viewportWithoutVirtualKeyboard: {
         width: viewportWidthWithoutVirtualKeyboard,
-        height: viewportHeightWithoutVirtualKeyboard
+        height: viewportHeightWithoutVirtualKeyboard,
       },
       currentViewport: {
         width: currentViewportWidth,
-        height: currentViewportHeight
+        height: currentViewportHeight,
       },
       virtualKeyboard: {
         width: currentViewportWidth,
-        height: viewportHeightWithoutVirtualKeyboard - currentViewportHeight
-      }
+        height: viewportHeightWithoutVirtualKeyboard - currentViewportHeight,
+      },
     };
   }
 
@@ -201,27 +229,26 @@ var virtualKeyboardDetector = function( window, undefined ) {
     unsubscribe: off,
     trigger: trigger,
     publish: trigger,
-    dispatchEvent: trigger
+    dispatchEvent: trigger,
   };
+};
 
-}
-
-var noop = () => { };
+var noop = () => {};
 
 // Export an object with noops when not on web.
 export default typeof document !== "undefined"
   ? virtualKeyboardDetector(window)
   : {
-    init: noop,
-    isVirtualKeyboardVisible: noop,
-    getVirtualKeyboardSize: noop,
-    on: noop,
-    addEventListener: noop,
-    subscribe: noop,
-    off: noop,
-    removeEventListener: noop,
-    unsubscribe: noop,
-    trigger: noop,
-    publish: noop,
-    dispatchEvent: noop,
-  }
+      init: noop,
+      isVirtualKeyboardVisible: noop,
+      getVirtualKeyboardSize: noop,
+      on: noop,
+      addEventListener: noop,
+      subscribe: noop,
+      off: noop,
+      removeEventListener: noop,
+      unsubscribe: noop,
+      trigger: noop,
+      publish: noop,
+      dispatchEvent: noop,
+    };
